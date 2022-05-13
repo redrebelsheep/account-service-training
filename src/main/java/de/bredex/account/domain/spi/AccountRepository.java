e64139070de522564fe2b698942690e219d6d6d6
Timo Rohrberg <timo.rohrberg.external@gropyus.com>
2022-05-13 18:49:35 +0200

    Fixed type of Id in repository.

--- src/main/java/de/bredex/account/domain/spi/AccountRepository.java
+++ src/main/java/de/bredex/account/domain/spi/AccountRepository.java
@@ -4,4 +4,4 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccountRepository extends CrudRepository<AccountDao, String>{}
+public interface AccountRepository extends CrudRepository<AccountDao, Integer>{}
